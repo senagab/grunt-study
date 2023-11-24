27c67b7e2f423b3e172206bc517c4093c9535ae5
senagab <senagab>
2023-11-24 19:23:35 -0400

    feat: tarefa default e async

--- gruntfile.js
+++ gruntfile.js
@@ -2,4 +2,14 @@ module.exports = function (grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'), // objeto de configuração
     })
+
+    grunt.registerTask('olagrunt', function () {
+        const done = this.async();
+        setTimeout(function () {
+            console.log('ola grunt!');
+            done();
+        }, 3000)
+    })
+
+    grunt.registerTask('default', ['olagrunt'])
 }
