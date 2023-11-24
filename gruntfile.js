8169047d0c190ddacd44f833d438eaa365c53720
senagab <senagab>
2023-11-24 19:59:36 -0400

    feat: grun watch

--- gruntfile.js
+++ gruntfile.js
@@ -15,11 +15,18 @@ module.exports = function (grunt) {
                     'dist/styles/main.min.css': 'src/styles/main.less' //arquivo de destino: arquivo de origem
                 }
             }
+        },
+        watch: {
+            less: {
+                files: ['src/styles/**/*.less'],
+                tasks: ['less:development']
+            }
         }
     })
 
     grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
-    grunt.registerTask('default', ['less:development'])
+    grunt.registerTask('default', ['watch'])
     grunt.registerTask('build', ['less:production'])
 }
