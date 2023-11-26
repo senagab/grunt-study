ab48d32b2360daa6c60efa2067c5ea7e2aaf35b9
senagab <senagab@gmail.com>
2023-11-26 09:52:59 -0400

    feat: commit inicial

--- gruntfile.js
+++ gruntfile.js
@@ -1,94 +1,13 @@
-module.exports = function (grunt) {
+module.exports = function(grunt) {
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'), // objeto de configuração
+        pkg: grunt.file.readJSON('package.json'),
         less: {
             development: {
                 files: {
-                    'dev/styles/main.css': 'src/styles/main.less'
-                }
-            },
-            production: {
-                options: {
-                    compress: true
-                },
-                files: {
-                    'dist/styles/main.min.css': 'src/styles/main.less' //arquivo de destino: arquivo de origem
+                    'dist/styles/main.css': 'src/styles/main.less'
                 }
             }
         },
-        watch: {
-            less: {
-                files: ['src/styles/**/*.less'],
-                tasks: ['less:development']
-            },
-            html: {
-                files: ['src/index.html'],
-                tasks: ['replace:dev']
-            }
-        },
-        replace: {
-            dev: {
-                options: {
-                    patterns: [
-                        {
-                            match: 'ENDERECO_DO_CSS',
-                            replacement: './styles/main.css'
-                        },
-                        {
-                            match: 'ENDERECO_DO_JS',
-                            replacement: '../src/scripts/main.js'
-                        }
-                    ]
-                },
-                files: [
-                    {
-                        expand: true,
-                        flatten: true,
-                        src: ['src/index.html'],
-                        dest: 'dev/'
-
-                    }
-                ]
-            },
-            dist: {
-                options: {
-                    patterns: [
-                        {
-                            match: 'ENDERECO_DO_CSS',
-                            replacement: './styles/main.min.css'
-                        },
-                        
-                        {
-                            match: 'ENDERECO_DO_JS',
-                            replacement: './scripts/main.min.js'
-                        }
-                    ]
-                },
-                files: [
-                    {
-                        expand: true,
-                        flatten: true,
-                        src: ['prebuild/index.html'],
-                        dest: 'dist/'
-
-                    }
-                ]
-            }
-        },
-        htmlmin: {
-            dist: {
-                options: {
-                    removeComments: true,
-                    collapseWhitespace: true,
-                },
-                files: {
-                    // 1 - minificação
-                    // 2 - substituição
-                    'prebuild/index.html': 'src/index.html'
-                }
-            }
-        },
-        clean: ['prebuild'],
         uglify: {
             target: {
                 files: {
@@ -99,12 +18,7 @@ module.exports = function (grunt) {
     })
 
     grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-replace');
-    grunt.loadNpmTasks('grunt-contrib-htmlmin');
-    grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-uglify');
 
-    grunt.registerTask('default', ['watch'])
-    grunt.registerTask('build', ['less:production', 'htmlmin:dist', 'replace:dist', 'clean', 'uglify'])
+    grunt.registerTask('default', ['less', 'uglify']);
 }
